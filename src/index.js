630d9eb992af69c17ff0a74db7718dfe309dfa4c
Sanjitha Singh <sanjitha.singh@gmail.com>
2020-11-21 21:32:29 +0530

    Allow access from netlify

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const options = {
   playground: "/playground",
   cors: {
     credentials: true,
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://courserate.netlify.app/"],
   },
 };
 
